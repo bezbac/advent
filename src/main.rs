f632fcefe0eeb23b20f7337fa1278a39352d740a
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2024-12-01 11:31:33 +0100

    Clean up the code

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,53 @@
+use anyhow::Result;
 use std::{collections::HashMap, fs};
 
-fn main() {
+fn main() -> Result<()> {
     let input = fs::read_to_string("./inputs/day1.txt").expect("Failed to read file");
 
-    let numbers: Vec<(usize, usize)> = input
-        .lines()
-        .map(|line| {
-            let mut split = line.trim().split("   ");
+    let mut a = Vec::new();
+    let mut b = Vec::new();
 
-            let a = split.next().unwrap();
-            let b = split.next().unwrap();
+    for line in input.lines() {
+        let mut split = line.trim().split("   ");
 
-            let a: usize = a.parse().unwrap();
-            let b: usize = b.parse().unwrap();
+        let a_num: isize = split
+            .next()
+            .expect("Could not read first number in line")
+            .parse()?;
 
-            (a, b)
-        })
-        .collect();
+        let b_num: isize = split
+            .next()
+            .expect("Could not read second number in line")
+            .parse()?;
 
-    let mut a: Vec<usize> = numbers.clone().into_iter().map(|(a, _)| a).collect();
-    let mut b: Vec<usize> = numbers.clone().into_iter().map(|(_, b)| b).collect();
-
-    a.sort();
-    b.sort();
-
-    let result: isize = a
-        .iter()
-        .zip(b.iter())
-        .map(|(a, b)| (*a as isize - *b as isize).abs())
-        .sum();
-
-    println!("Result (Part 1): {}", result);
-
-    let mut occurences_of_a_in_b: HashMap<usize, usize> = HashMap::new();
-
-    for x in b {
-        let entry = occurences_of_a_in_b.entry(x).or_insert(0);
-        *entry += 1;
+        a.push(a_num);
+        b.push(b_num);
     }
+
+    a.sort_unstable();
+    b.sort_unstable();
+
+    let result: isize = a.iter().zip(b.iter()).map(|(a, b)| (a - b).abs()).sum();
+
+    println!("Result (Part 1): {result}");
+
+    let occurences_of_a_in_b: HashMap<isize, isize> =
+        b.iter().fold(HashMap::new(), |mut acc, x| {
+            let entry = acc.entry(*x).or_insert(0);
+            *entry += 1;
+            acc
+        });
 
     let result: isize = a
         .iter()
         .map(|x| {
             // Multiply the number from a with the number of occurences in b
-            let factor = *occurences_of_a_in_b.get(x).unwrap_or(&0) as isize;
-            *x as isize * factor
+            let factor = occurences_of_a_in_b.get(x).unwrap_or(&0);
+            x * factor
         })
         .sum();
 
-    println!("Result (Part 2): {}", result);
+    println!("Result (Part 2): {result}");
+
+    Ok(())
 }
