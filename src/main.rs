68d2cc749f10f70752528bf550c90fd2c7e5c253
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2024-12-02 08:43:05 +0100

    Solve day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -29,25 +29,69 @@ fn check_if_nums_are_safe(nums: &[isize]) -> bool {
     true
 }
 
+fn check_if_nums_are_safe_with_dampener(nums: &[isize]) -> bool {
+    if check_if_nums_are_safe(nums) {
+        return true;
+    }
+
+    let mut safe_permutations: Vec<usize> = Vec::new();
+
+    for i in 0..nums.len() {
+        let nums_without_i = nums
+            .iter()
+            .enumerate()
+            .filter(|(j, _)| *j != i)
+            .map(|(_, x)| *x)
+            .collect::<Vec<isize>>();
+
+        let is_safe = check_if_nums_are_safe(&nums_without_i);
+
+        if is_safe {
+            safe_permutations.push(i);
+        }
+    }
+
+    if safe_permutations.len() == 0 {
+        return false;
+    }
+
+    true
+}
+
 fn main() {
     let input = fs::read_to_string("./inputs/day2.txt").expect("Failed to read file");
 
+    let parsed: Vec<Vec<isize>> = input
+        .lines()
+        .map(|line| {
+            line.trim()
+                .split(' ')
+                .map(|x| x.parse::<isize>().unwrap())
+                .collect()
+        })
+        .collect();
+
     let mut safe = 0;
-    for line in input.lines() {
-        let nums: Vec<isize> = line
-            .trim()
-            .split(' ')
-            .map(|x| x.parse::<isize>().unwrap())
-            .collect();
-
+    for nums in &parsed {
         let is_safe = check_if_nums_are_safe(&nums);
-
         if is_safe {
             safe += 1;
         }
     }
 
     println!("Result (Part 1): {safe}");
+
+    // Part 2
+
+    let mut safe = 0;
+    for nums in &parsed {
+        let is_safe = check_if_nums_are_safe_with_dampener(&nums);
+        if is_safe {
+            safe += 1;
+        }
+    }
+
+    println!("Result (Part 2): {safe}");
 }
 
 #[cfg(test)]
@@ -74,5 +118,17 @@ mod tests {
         assert_eq!(check_if_nums_are_safe(&[7, 6, 4, 2, 1]), true);
         assert_eq!(check_if_nums_are_safe(&[94, 96, 97, 98, 99]), true);
         assert_eq!(check_if_nums_are_safe(&[1, 3, 6, 7, 9]), true);
+    }
+
+    #[test]
+    fn dampener() {
+        assert_eq!(check_if_nums_are_safe_with_dampener(&[0, 0, 0, 0]), false);
+        assert_eq!(check_if_nums_are_safe_with_dampener(&[1, 2, 3, 4]), true);
+        assert_eq!(check_if_nums_are_safe_with_dampener(&[10, 6, 5, 4]), true);
+        assert_eq!(check_if_nums_are_safe_with_dampener(&[6, 6, 5, 4]), true);
+        assert_eq!(check_if_nums_are_safe_with_dampener(&[10, 9, 12, 8]), true);
+        assert_eq!(check_if_nums_are_safe_with_dampener(&[10, 9, -12, 8]), true);
+        assert_eq!(check_if_nums_are_safe_with_dampener(&[10, 9, 8, 7]), true);
+        assert_eq!(check_if_nums_are_safe_with_dampener(&[10, 9, 8, -7]), true);
     }
 }
