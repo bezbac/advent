f953e33bf2b4cf1389087e17366228ab47a7400e
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2024-12-19 20:40:30 +0100

    Solve day 19 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn can_be_combined(output: &str, parts: &[&str]) -> bool {
     if output.len() < 1 {
@@ -14,6 +14,39 @@ fn can_be_combined(output: &str, parts: &[&str]) -> bool {
     }
 
     return false;
+}
+
+fn get_possible_combination_count<'w, 'p>(
+    cache: &mut HashMap<&'w str, usize>,
+    output: &'w str,
+    parts: &[&'p str],
+) -> usize {
+    let entry = cache.get(output);
+
+    if let Some(result) = entry {
+        return result.clone();
+    }
+
+    let mut result = 0;
+
+    for part in parts {
+        if output.starts_with(part) {
+            let remaining = &output[part.len()..output.len()];
+
+            if remaining.len() == 0 {
+                result += 1;
+                continue;
+            }
+
+            let childs = get_possible_combination_count(cache, remaining, parts);
+
+            result += childs;
+        }
+    }
+
+    cache.insert(output, result.clone());
+
+    return result;
 }
 
 fn main() {
@@ -39,6 +72,19 @@ fn main() {
         .len();
 
     println!("Result (Part 1): {result}");
+
+    let mut cache = HashMap::new();
+
+    let result: usize = words
+        .iter()
+        .enumerate()
+        .map(|(i, word)| {
+            println!("Iteration {i}");
+            get_possible_combination_count(&mut cache, word, &parts)
+        })
+        .sum();
+
+    println!("Result (Part 2): {result}");
 }
 
 #[cfg(test)]
@@ -57,5 +103,46 @@ mod tests {
         assert_eq!(can_be_combined("bwurrg", &parts), true);
         assert_eq!(can_be_combined("brgr", &parts), true);
         assert_eq!(can_be_combined("bbrgwb", &parts), false);
+    }
+
+    #[test]
+    fn test_get_possible_combinations() {
+        let parts = ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"];
+
+        let mut cache = HashMap::new();
+
+        assert_eq!(
+            get_possible_combination_count(&mut cache, "brwrr", &parts),
+            2
+        );
+
+        assert_eq!(
+            get_possible_combination_count(&mut cache, "bggr", &parts),
+            1
+        );
+        assert_eq!(
+            get_possible_combination_count(&mut cache, "gbbr", &parts),
+            4
+        );
+        assert_eq!(
+            get_possible_combination_count(&mut cache, "rrbgbr", &parts),
+            6
+        );
+        assert_eq!(
+            get_possible_combination_count(&mut cache, "ubwu", &parts),
+            0
+        );
+        assert_eq!(
+            get_possible_combination_count(&mut cache, "bwurrg", &parts),
+            1
+        );
+        assert_eq!(
+            get_possible_combination_count(&mut cache, "brgr", &parts),
+            2
+        );
+        assert_eq!(
+            get_possible_combination_count(&mut cache, "bbrgwb", &parts),
+            0
+        );
     }
 }
