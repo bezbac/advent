701bb45c564a639283c37a1baa3659b0c9645c59
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2024-12-20 08:43:46 +0100

    Solve day 20 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,224 @@
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
-fn can_be_combined(output: &str, parts: &[&str]) -> bool {
-    if output.is_empty() {
-        return true;
-    }
+use pathfinding::prelude::astar_bag_collect;
 
-    for part in parts {
-        if output.starts_with(part) && can_be_combined(output.trim_start_matches(part), parts) {
-            return true;
-        }
-    }
-
-    false
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tile {
+    Empty,
+    Wall,
 }
 
-fn get_possible_combination_count<'w>(
-    cache: &mut HashMap<&'w str, usize>,
-    output: &'w str,
-    parts: &[&str],
-) -> usize {
-    let entry = cache.get(output);
+#[derive(Debug, Clone)]
+struct Map {
+    tiles: Vec<Vec<Tile>>,
+    start: (usize, usize),
+    end: (usize, usize),
+}
 
-    if let Some(result) = entry {
-        return *result;
+impl Map {
+    fn width(&self) -> usize {
+        self.tiles.first().unwrap().len()
     }
 
-    let mut result = 0;
+    fn height(&self) -> usize {
+        self.tiles.len()
+    }
 
-    for part in parts {
-        if output.starts_with(part) {
-            let remaining = &output[part.len()..output.len()];
+    fn parse(input: &str) -> Map {
+        let input = input.trim();
 
-            if remaining.is_empty() {
-                result += 1;
-                continue;
+        let mut tiles = Vec::new();
+        let mut start = None;
+        let mut end = None;
+
+        for (y, row) in input.lines().enumerate() {
+            let mut row_tiles = Vec::new();
+
+            for (x, c) in row.chars().enumerate() {
+                match c {
+                    '#' => {
+                        row_tiles.push(Tile::Wall);
+                    }
+                    '.' => {
+                        row_tiles.push(Tile::Empty);
+                    }
+                    'S' => {
+                        row_tiles.push(Tile::Empty);
+                        start = Some((x, y));
+                    }
+                    'E' => {
+                        row_tiles.push(Tile::Empty);
+                        end = Some((x, y));
+                    }
+                    _ => panic!("Unexpected character"),
+                }
             }
 
-            let childs = get_possible_combination_count(cache, remaining, parts);
+            tiles.push(row_tiles);
+        }
 
-            result += childs;
+        let start = start.unwrap();
+        let end = end.unwrap();
+
+        Map { tiles, start, end }
+    }
+}
+
+struct Run {
+    map: Map,
+    remaining_cheats: usize,
+    forbidden_cheat_positions: HashMap<usize, HashSet<(usize, usize)>>,
+}
+
+impl Run {
+    fn next_positions(&self, &(x, y): &(usize, usize)) -> Vec<(usize, usize)> {
+        [
+            (x as isize, y as isize - 1),
+            (x as isize, y as isize + 1),
+            (x as isize - 1, y as isize),
+            (x as isize + 1, y as isize),
+        ]
+        .into_iter()
+        .filter_map(|(x, y)| -> Option<(usize, usize)> {
+            let is_within_bounds = x >= 0
+                && y >= 0
+                && (x as usize) < self.map.width()
+                && (y as usize) < self.map.height();
+
+            if !is_within_bounds {
+                return None;
+            }
+
+            let x = x as usize;
+            let y = y as usize;
+
+            if let Tile::Wall = self.map.tiles[y][x] {
+                if self.remaining_cheats == 2
+                    && self
+                        .forbidden_cheat_positions
+                        .get(&2)
+                        .unwrap_or(&HashSet::new())
+                        .contains(&(x, y))
+                {
+                    return None;
+                }
+
+                // FIXME: A cheat is only allowed *exactly once*
+                if self.remaining_cheats == 1
+                    && self
+                        .forbidden_cheat_positions
+                        .get(&1)
+                        .unwrap_or(&HashSet::new())
+                        .contains(&(x, y))
+                {
+                    return None;
+                }
+
+                if self.remaining_cheats == 0 {
+                    return None;
+                }
+            }
+
+            Some((x, y))
+        })
+        .collect()
+    }
+
+    fn find_shortest_paths(&self) -> Option<(Vec<Vec<(usize, usize)>>, usize)> {
+        let result = astar_bag_collect(
+            &self.map.start,
+            |position| {
+                let successors = self
+                    .next_positions(position)
+                    .iter()
+                    .map(|(nx, ny)| ((*nx, *ny), 1))
+                    .collect::<Vec<_>>();
+
+                successors
+            },
+            |&(x, y)| {
+                let (ex, ey) = self.map.end;
+
+                (((ex as isize - x as isize).pow(2) + (ey as isize - y as isize).pow(2)) as f64)
+                    .sqrt() as usize
+            },
+            |position| position == &self.map.end,
+        );
+
+        result
+    }
+}
+
+fn find_cheat_positions(map: &Map, path: &[(usize, usize)]) -> Vec<(usize, usize)> {
+    path.iter()
+        .filter(|&(x, y)| map.tiles[*y][*x] == Tile::Wall)
+        .copied()
+        .collect()
+}
+
+fn find_cheats(map: Map) -> (usize, HashMap<usize, usize>) {
+    let baseline = Run {
+        map: map.clone(),
+        remaining_cheats: 0,
+        forbidden_cheat_positions: HashMap::new(),
+    };
+
+    let baseline = baseline.find_shortest_paths().unwrap().1;
+
+    let mut result = HashMap::new();
+    let mut forbidden_cheat_positions: HashMap<usize, HashSet<(usize, usize)>> = HashMap::new();
+
+    loop {
+        let cheat_run = Run {
+            map: map.clone(),
+            remaining_cheats: 2,
+            forbidden_cheat_positions: forbidden_cheat_positions.clone(),
+        };
+
+        let paths = cheat_run.find_shortest_paths();
+
+        let Some((paths, cost)) = paths else {
+            break;
+        };
+
+        if cost == baseline {
+            break;
+        }
+
+        let entry = result.entry(baseline.abs_diff(cost)).or_default();
+
+        *entry += paths.len();
+
+        for path in paths {
+            let cheat_positions = find_cheat_positions(&map, &path);
+            let mut cheat_positions = cheat_positions.iter();
+
+            if let Some(position) = cheat_positions.next() {
+                let entry = forbidden_cheat_positions.entry(1).or_default();
+                entry.insert(*position);
+            }
+
+            if let Some(position) = cheat_positions.next() {
+                let entry = forbidden_cheat_positions.entry(2).or_default();
+                entry.insert(*position);
+            }
         }
     }
 
-    cache.insert(output, result);
-
-    result
+    (baseline, result)
 }
 
 fn main() {
-    let input = fs::read_to_string("./inputs/day19.txt").expect("Failed to read file");
+    let input = fs::read_to_string("./inputs/day20.txt").expect("Failed to read file");
 
-    let input = input.trim();
+    let map = Map::parse(&input);
 
-    let mut lines = input.lines();
-
-    let parts: Vec<&str> = lines.next().unwrap().split(", ").collect();
-
-    lines.next();
-
-    let words: Vec<_> = lines.collect();
-
-    let result = words
-        .iter()
-        .filter(|word| can_be_combined(word, &parts))
-        .collect::<Vec<_>>()
-        .len();
+    let result = 0;
 
     println!("Result (Part 1): {result}");
-
-    let mut cache = HashMap::new();
-
-    let result: usize = words
-        .iter()
-        .enumerate()
-        .map(|(i, word)| {
-            println!("Iteration {i}");
-            get_possible_combination_count(&mut cache, word, &parts)
-        })
-        .sum();
-
-    println!("Result (Part 2): {result}");
 }
 
 #[cfg(test)]
@@ -87,57 +226,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_can_be_combined() {
-        let parts = ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"];
+    fn test_example() {
+        let input = r#"
+###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############
+        "#;
 
-        assert_eq!(can_be_combined("brwrr", &parts), true);
-        assert_eq!(can_be_combined("bggr", &parts), true);
-        assert_eq!(can_be_combined("gbbr", &parts), true);
-        assert_eq!(can_be_combined("rrbgbr", &parts), true);
-        assert_eq!(can_be_combined("ubwu", &parts), false);
-        assert_eq!(can_be_combined("bwurrg", &parts), true);
-        assert_eq!(can_be_combined("brgr", &parts), true);
-        assert_eq!(can_be_combined("bbrgwb", &parts), false);
-    }
+        let map = Map::parse(input);
 
-    #[test]
-    fn test_get_possible_combinations() {
-        let parts = ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"];
+        let (baseline, cheats) = find_cheats(map);
 
-        let mut cache = HashMap::new();
+        assert_eq!(baseline, 84);
 
-        assert_eq!(
-            get_possible_combination_count(&mut cache, "brwrr", &parts),
-            2
-        );
-
-        assert_eq!(
-            get_possible_combination_count(&mut cache, "bggr", &parts),
-            1
-        );
-        assert_eq!(
-            get_possible_combination_count(&mut cache, "gbbr", &parts),
-            4
-        );
-        assert_eq!(
-            get_possible_combination_count(&mut cache, "rrbgbr", &parts),
-            6
-        );
-        assert_eq!(
-            get_possible_combination_count(&mut cache, "ubwu", &parts),
-            0
-        );
-        assert_eq!(
-            get_possible_combination_count(&mut cache, "bwurrg", &parts),
-            1
-        );
-        assert_eq!(
-            get_possible_combination_count(&mut cache, "brgr", &parts),
-            2
-        );
-        assert_eq!(
-            get_possible_combination_count(&mut cache, "bbrgwb", &parts),
-            0
-        );
+        dbg!(cheats);
     }
 }
